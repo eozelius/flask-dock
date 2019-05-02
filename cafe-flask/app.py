7d7c5baad390ac44e1dc27e4430b8a63e044a911
ethan ozelius <e.ozelius@gmail.com>
2019-05-01 21:02:32 -0400

    whitelist fronend app

--- cafe-flask/app.py
+++ cafe-flask/app.py
@@ -1,23 +1,15 @@
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
-
-'''
-  app = Flask(__name__)
-  CORS(app)
-
-  @app.route("/")
-  def helloWorld():
-    return "Hello, cross-origin-world!"
-
-'''
+import os
 
 app = Flask(__name__)
-CORS(app, resources={r"*": {"origins": "http://localhost:8080"}})
+frontend_ip = os.environ['FRONTEND_IP']
+CORS(app, resources={r"*": {"origins": frontend_ip}})
 
 @app.route('/')
 def root():
-  return jsonify('This text is being served by Flask.')
+  return jsonify(frontend_ip)
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
